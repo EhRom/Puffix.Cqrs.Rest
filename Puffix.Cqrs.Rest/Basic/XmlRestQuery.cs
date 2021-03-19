06572e49cad3979b7d1e09c52960406b0707db90
Romain EHRET <romain.ehret@sword-group.com>
2021-03-19 20:37:29 +0100

    Add project files.

---
+++ Puffix.Cqrs.Rest/Basic/XmlRestQuery.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace Puffix.Cqrs.Rest.Basic
+{
+    /// <summary>
+    /// Implémentation de base pour la définition d'une requête REST avec un résultat au format XML.
+    /// </summary>
+    /// <typeparam name="ResultT">Type du résutlat de la reuqête REST.</typeparam>
+    public abstract class XmlRestQuery<ResultT> : RestQuery<ResultT, XElement>
+    {
+        #region Propriétés
+
+        /// <summary>
+        /// Interprétation du résultat de la requête.
+        /// </summary>
+        protected override Func<string, XElement> ParseResult => restResult => XElement.Parse(restResult);
+
+        #endregion Propriétés
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# Puffix CQRS Rest
+
+REST extension for the [Puffix CQRS framework](https://github.com/EhRom/Puffix.Cqrs). Enable to manage REST Queries.
+
+[![NuGet version (Puffix.Cqrs.Rest)](https://img.shields.io/nuget/v/Puffix.Cqrs.Rest.svg?style=flat-square)](https://www.nuget.org/packages/Puffix.Cqrs.Rest/)
+[![Build status](https://github.com/EhRom/Puffix.Cqrs.Rest/workflows/.NET%20Core/badge.svg)](https://github.com/EhRom/Puffix.Cqrs.Rest/actions?query=workflow%3A%22.NET+Core%22)
